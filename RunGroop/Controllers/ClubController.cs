fbbe81b5c6b4b80fef05204907f6de26abfbf5e7
shakil mahmud <shakil1473@gmail.com>
2024-01-16 00:06:36 +0100

    Detail Page
    Added details page for the clubs and reces.

--- RunGroop/Controllers/ClubController.cs
+++ RunGroop/Controllers/ClubController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using RunGroop.Data;
+using RunGroop.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -22,6 +24,19 @@ namespace RunGroop.Controllers
         {
             var clubs = _applicationDbContext.Clubs.ToList();
             return View(clubs);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            Club club = _applicationDbContext.Clubs.FirstOrDefault(c => c.Id == id);
+
+            /*
+             * _applicationDbContext.Clubs.Include(a => a.Address).FirstOrDefault(c => c.Id == id);
+             * When one model has another model as properties we need to use include
+             * Include actually doing the join
+             */
+
+            return View(club);
         }
     }
 }
--- RunGroop/Controllers/RaceController.cs
+++ RunGroop/Controllers/RaceController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using RunGroop.Data;
+using RunGroop.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -21,6 +22,12 @@ namespace RunGroop.Controllers
         {
             var races = _applicationDbContext.Races.ToList();
             return View(races);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            Race race = _applicationDbContext.Races.FirstOrDefault(r => r.Id == id);
+            return View(race);
         }
     }
 }
